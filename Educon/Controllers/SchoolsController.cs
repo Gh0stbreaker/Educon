a972abc5b1a4fd05d5ff3253fc9967d29ac4531f
Gh0stbreaker <46496187+Gh0stbreaker@users.noreply.github.com>
2025-07-02 21:35:30 +0200

    Add generic repository pattern

---
+++ Educon/Controllers/SchoolsController.cs
@@ -0,0 +1,59 @@
+using Educon.Models;
+using Educon.Repositories;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Educon.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class SchoolsController : ControllerBase
+{
+    private readonly IRepository<School> _repository;
+
+    public SchoolsController(IRepository<School> repository)
+    {
+        _repository = repository;
+    }
+
+    [HttpGet]
+    public async Task<IEnumerable<School>> Get()
+    {
+        return await _repository.GetAllAsync();
+    }
+
+    [HttpGet("{id}")]
+    public async Task<ActionResult<School>> Get(Guid id)
+    {
+        var school = await _repository.GetByIdAsync(id);
+        if (school == null)
+        {
+            return NotFound();
+        }
+        return Ok(school);
+    }
+
+    [HttpPost]
+    public async Task<ActionResult<School>> Post(School school)
+    {
+        await _repository.AddAsync(school);
+        return CreatedAtAction(nameof(Get), new { id = school.Id }, school);
+    }
+
+    [HttpPut("{id}")]
+    public async Task<IActionResult> Put(Guid id, School school)
+    {
+        if (id != school.Id)
+        {
+            return BadRequest();
+        }
+        await _repository.UpdateAsync(school);
+        return NoContent();
+    }
+
+    [HttpDelete("{id}")]
+    public async Task<IActionResult> Delete(Guid id)
+    {
+        await _repository.DeleteAsync(id);
+        return NoContent();
+    }
+}
---
+++ Educon/Repositories/IRepository.cs
@@ -0,0 +1,13 @@
+using System.Linq.Expressions;
+
+namespace Educon.Repositories;
+
+public interface IRepository<T> where T : class
+{
+    Task<IEnumerable<T>> GetAllAsync();
+    Task<T?> GetByIdAsync(Guid id);
+    Task<T> AddAsync(T entity);
+    Task UpdateAsync(T entity);
+    Task DeleteAsync(Guid id);
+    Task<IEnumerable<T>> FindAsync(Expression<Func<T, bool>> predicate);
+}
