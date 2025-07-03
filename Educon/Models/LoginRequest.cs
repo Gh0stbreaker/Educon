4dde4c0d437be1732ddda35226fcfffc2c0cf60d
Gh0stbreaker <46496187+Gh0stbreaker@users.noreply.github.com>
2025-07-03 13:18:33 +0200

    Improve login logic with Identity

--- Educon/Models/LoginRequest.cs
+++ Educon/Models/LoginRequest.cs
@@ -2,6 +2,10 @@ namespace Educon.Models;
 
 public class LoginRequest
 {
+    [System.ComponentModel.DataAnnotations.Required]
+    [System.ComponentModel.DataAnnotations.EmailAddress]
     public string Email { get; set; } = string.Empty;
+
+    [System.ComponentModel.DataAnnotations.Required]
     public string Password { get; set; } = string.Empty;
 }
